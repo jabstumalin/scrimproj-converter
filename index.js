b68292322bf21869c12d90713d58de853837555f
jabstumalin <jetumalin@catsu.edu.ph>
2025-03-02 03:09:50 +0800

    js added

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const inputEl = document.getElementById('input');
+
+let lengthEl = document.getElementById('length-el');
+let volEl = document.getElementById('vol-el');
+let weightEl = document.getElementById('weight-el');
+
+function meterFeet() {
+    let input = inputEl.value;
+    
+    let feet = input * 3.281;
+    let meter = input / 3.281;
+    
+    let gallon = input * 0.264;
+    let liter = input / 0.264;
+
+    let pound = input * 2.204;
+    let kilogram = input / 2.204;
+
+    lengthEl.textContent = input + " meters" + " = " + feet + " feet" + " | " + input + " feet" + " = " + meter.toFixed(3) + " meters";
+    volEl.textContent = input + " liters" + " = " + gallon + " gallon" + " | " + input + " gallon" + " = " + liter.toFixed(3) + " liters";
+    weightEl.textContent = input + " kilos" + " = " + pound.toFixed(3) + " pounds" + " | " + input + " pound" + " = " + kilogram.toFixed(3) + " kilos";
+}
